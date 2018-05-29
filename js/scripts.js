6e559c121b5bb217e049fee2a121654fda65a6d4
Steven Colburn and William Kinzig <steven@>
2018-05-29 11:40:44 -0700

    add light and dark buttons to change background

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 $(function()
 {
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
 
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
 });
